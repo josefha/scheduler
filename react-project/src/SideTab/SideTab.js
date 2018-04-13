98e1a2c8d92f31f12c4ef1aa4e955c47d9e7e977
Josef Karakoca <josef@Josefs-MacBook-Pro.local>
2018-04-13 16:15:23 +0200

    removed the headtag from mergeconflict in SideTab.js

--- react-project/src/SideTab/SideTab.js
+++ react-project/src/SideTab/SideTab.js
@@ -6,19 +6,12 @@ import './SideTab.css';
 
 class SideTab extends Component {
   render() {
-<<<<<<< HEAD
     return (
-      <div id="SideTab">
-        SideTab
-      </div>
-    );
-=======
-    return ( <div className='SideTab'>
-    <h2> SideTab </h2>
-    <Staff/>
+      <div className='SideTab'>
+        <h2> SideTab </h2>
+        <Staff/>
         <SearchBar/>
     </div>);
->>>>>>> 9ccb4a6020ea1a304d92ae758b86cef7299cf097
   }
 }
 
