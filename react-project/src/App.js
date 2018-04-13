07e025636a7bcb19b2f5b915f3696016bb0b894f
Josef Karakoca <josef@Josefs-MacBook-Pro.local>
2018-04-13 15:17:07 +0200

    added js files and some react structre

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Header from './Header/Header'
+import MainContent from './MainContent/MainContent'
+import SideTab from './SideTab/SideTab'
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
       <h1> Hello world </h1>
+      <Header/>
+      <MainContent/>
+      <SideTab/>
       </div>
     );
   }
