bbd33ce366e69ab0a31c12d5404eca66b981348e
Alex Wahlandt <alex.wahlandt@gmail.com>
2018-04-16 13:47:49 +0200

    implementing shifts and hours

--- react-project/src/MainContent/CalenderPage/CalenderDay.js
+++ react-project/src/MainContent/CalenderPage/CalenderDay.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-import './CalenderPage.css'
+import './CalenderPage.css';
+import Hour from './Hour';
 
 export default class CalenderDay extends Component {
   constuctor(){
     this.super();
+    this.state={
+      shifts = [];
+    }
 
   }
 
   render(){
     return(
-    <div className="CalenderDay">{this.props.name}</div>
+    <div className="CalenderDay">
+        {this.props.name}
+        <Hour/>
+    </div>
+
   );
   }
 }
---
+++ react-project/src/MainContent/CalenderPage/Hour.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './CalenderPage.css'
+
+export default class Hour extends Component {
+  constuctor(){
+    this.super();
+  }
+
+  render(){
+    return(
+      <div>Hour</div>
+  );
+  }
+}
