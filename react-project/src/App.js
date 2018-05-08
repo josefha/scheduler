86d1aad17097311ac604df7314158ad7ec478f09
joseph13g <joseph13g@gmail.com>
2018-05-08 16:47:11 +0200

    Added first version of login page

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -6,12 +6,16 @@ import MainContent from './MainContent/MainContent'
 import SideTab from './SideTab/SideTab'
 
 import {Grid, Row, Col} from 'react-bootstrap';
+import Login from "./Login";
 //hello test
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {view: "calenderPage"}
+    this.state = {view: "calenderPage",
+    authorized:"false",
+        email:""
+    }
   }
 
   handleClick = (click) => {
@@ -20,23 +24,44 @@ class App extends Component {
     });
   }
 
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-        <Grid  className="container-fluid">
-          <Row className="show-grid no-gutter">
-            <Col xs={12} lg={2}>
-              <SideTab event = {this.handleClick} />
-            </Col>
-            <Col xs={12} lg={10}>
-              <MainContent view={this.state.view}/>
-            </Col>
-          </Row>
-        </Grid>
+    handleAuthorized = (a,b) => {
+        this.setState({
+            authorized: a,
+            email:b
+        });
+    }
 
-      </div>
-    );
+  returnCurrentView(){
+    if(this.state.authorized ===true){
+      return (
+          <div className="App">
+              <Header/>
+              <Grid  className="container-fluid">
+                  <Row className="show-grid no-gutter">
+                      <Col xs={12} lg={2}>
+                          <SideTab event = {this.handleClick} />
+                      </Col>
+                      <Col xs={12} lg={10}>
+                          <MainContent view={this.state.view}/>
+                      </Col>
+                  </Row>
+              </Grid>
+
+          </div>
+      );}
+      else{
+      return(<Login event = {this.handleAuthorized}/>)
+       // return(<Login/>)
+    }
+
+  }
+
+  render() {
+    return(
+    <div>
+        {this.returnCurrentView()}
+    </div>)
+
   }
 }
 
--- react-project/src/index.js
+++ react-project/src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './Login';
+import { BrowserRouter as Router } from "react-router-dom";
+
 import registerServiceWorker from './registerServiceWorker';
 /** Importing boostrap **/
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router>
+    <App />
+</Router>, document.getElementById('root'));
 registerServiceWorker();
